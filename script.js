51737ded15088ff9c19f9acfe9c65cd788309ef1
purnima11apr <141928600+purnima11apr@users.noreply.github.com>
2023-10-12 14:36:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 //complete this code
-class Person {}
+class Person {
+	 constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study() {
+        console.log(`${this.name} is studying`);
+    }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	 teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
